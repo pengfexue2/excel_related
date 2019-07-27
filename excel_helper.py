a6357da96fedf437d43b34733d4b29e82c8ea002
TED <32763978+pengfexue2@users.noreply.github.com>
2019-07-27 08:17:01 +0800

    Update excel_helper.py

--- excel_helper.py
+++ excel_helper.py
@@ -16,7 +16,13 @@ print(source)
 print(filter_merge)
 
 combine = pd.merge(group,filter_merge,on="角色")
-combine.insert(1,"数据K/60",round(filter_merge["数据K"]/60,2))
+
+#此处注释掉的是写文章时犯错的代码，filter_merge是对 source.csv只筛选了特定列的数据，看行数的话要比 combine 多一些；
+#combine.insert(1,"数据K/60",round(filter_merge["数据K"]/60,2))
+
+#最终要添加的“数据K/60”这列数据，其“数据K”来源应该是从表格融合完毕后的 combine 中取，这样才能做到每条数字一一对应
+combine.insert(1,"数据K/60",round(combine["数据K"]/60,2))
+
 print(combine)
 
 combine.to_excel(excel_writer="result.xlsx",index=False)
